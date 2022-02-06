5c5f97fa1288f76edb6452d6700f9be1fbe560f7
Vineeth pawar <vineethpawar99@gmail.com>
2022-02-06 15:51:22 +0530

    chore: adding react navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['prettier', '@react-native-community'],
+  plugins: ["prettier"],
+  rules: {
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off"
+  }
 };
